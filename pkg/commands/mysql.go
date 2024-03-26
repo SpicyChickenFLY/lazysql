d5ca6993a6ffe1fd4f8d1b257ce9894b7f2dfc2b
SpicyChickenFLY <xixiayuchao@gmail.com>
2024-03-26 22:06:57 +0800

    add new command for sql

---
+++ pkg/commands/mysql.go
@@ -0,0 +1,442 @@
+package commands
+
+import (
+	"database/sql"
+	"fmt"
+	"strconv"
+	"strings"
+
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/xo/dburl"
+)
+
+type MySQLCommand struct {
+	Connection *sql.DB
+	Provider   string
+}
+
+func (db *MySQLCommand) TestConnection(urlstr string) (err error) {
+	return db.Connect(urlstr)
+}
+
+func (db *MySQLCommand) Connect(urlstr string) (err error) {
+	db.SetProvider("mysql")
+
+	db.Connection, err = dburl.Open(urlstr)
+	if err != nil {
+		return err
+	}
+
+	err = db.Connection.Ping()
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (db *MySQLCommand) GetDatabases() ([]string, error) {
+	var databases []string
+
+	rows, err := db.Connection.Query("SHOW DATABASES")
+	if err != nil {
+		return databases, err
+	}
+
+	for rows.Next() {
+		var database string
+		err := rows.Scan(&database)
+		if err != nil {
+			return databases, err
+		}
+		if database != "information_schema" && database != "mysql" && database != "performance_schema" && database != "sys" {
+			databases = append(databases, database)
+		}
+	}
+
+	return databases, nil
+}
+
+func (db *MySQLCommand) GetTables(database string) (map[string][]string, error) {
+	rows, err := db.Connection.Query("SHOW TABLES FROM " + database)
+
+	tables := make(map[string][]string)
+
+	if err != nil {
+		return tables, err
+	}
+
+	for rows.Next() {
+		var table string
+		rows.Scan(&table)
+
+		tables[database] = append(tables[database], table)
+	}
+
+	return tables, nil
+}
+
+func (db *MySQLCommand) GetTableColumns(database, table string) (results [][]string, err error) {
+	rows, err := db.Connection.Query("DESCRIBE " + table)
+	if err != nil {
+		return results, err
+	}
+	defer rows.Close()
+
+	columns, err := rows.Columns()
+	if err != nil {
+		return results, err
+	}
+
+	results = append(results, columns)
+
+	for rows.Next() {
+		rowValues := make([]interface{}, len(columns))
+		for i := range columns {
+			rowValues[i] = new(sql.RawBytes)
+		}
+
+		rows.Scan(rowValues...)
+
+		var row []string
+		for _, col := range rowValues {
+			row = append(row, string(*col.(*sql.RawBytes)))
+		}
+
+		results = append(results, row)
+	}
+
+	return
+}
+
+func (db *MySQLCommand) GetConstraints(table string) (results [][]string, err error) {
+	splitTableString := strings.Split(table, ".")
+	database := splitTableString[0]
+	tableName := splitTableString[1]
+
+	rows, err := db.Connection.Query(fmt.Sprintf("SELECT CONSTRAINT_NAME, COLUMN_NAME, REFERENCED_TABLE_NAME, REFERENCED_COLUMN_NAME FROM information_schema.KEY_COLUMN_USAGE where TABLE_SCHEMA = '%s' AND TABLE_NAME = '%s'", database, tableName))
+	if err != nil {
+		return results, err
+	}
+
+	defer rows.Close()
+
+	columns, err := rows.Columns()
+	if err != nil {
+		return results, err
+	}
+
+	results = append(results, columns)
+
+	for rows.Next() {
+		rowValues := make([]interface{}, len(columns))
+		for i := range columns {
+			rowValues[i] = new(sql.RawBytes)
+		}
+
+		rows.Scan(rowValues...)
+
+		var row []string
+		for _, col := range rowValues {
+			row = append(row, string(*col.(*sql.RawBytes)))
+		}
+
+		results = append(results, row)
+	}
+
+	return
+}
+
+func (db *MySQLCommand) GetForeignKeys(table string) (results [][]string, err error) {
+	splitTableString := strings.Split(table, ".")
+	database := splitTableString[0]
+	tableName := splitTableString[1]
+
+	rows, err := db.Connection.Query(fmt.Sprintf("SELECT TABLE_NAME, COLUMN_NAME, CONSTRAINT_NAME, REFERENCED_COLUMN_NAME, REFERENCED_TABLE_NAME FROM information_schema.KEY_COLUMN_USAGE where REFERENCED_TABLE_SCHEMA = '%s' AND REFERENCED_TABLE_NAME = '%s'", database, tableName))
+	if err != nil {
+		return results, err
+	}
+
+	defer rows.Close()
+
+	columns, err := rows.Columns()
+	if err != nil {
+		return results, err
+	}
+
+	results = append(results, columns)
+
+	for rows.Next() {
+		rowValues := make([]interface{}, len(columns))
+		for i := range columns {
+			rowValues[i] = new(sql.RawBytes)
+		}
+
+		rows.Scan(rowValues...)
+
+		var row []string
+		for _, col := range rowValues {
+			row = append(row, string(*col.(*sql.RawBytes)))
+		}
+
+		results = append(results, row)
+	}
+
+	return
+}
+
+func (db *MySQLCommand) GetIndexes(table string) (results [][]string, err error) {
+	rows, err := db.Connection.Query("SHOW INDEX FROM " + table)
+	if err != nil {
+		return results, err
+	}
+	defer rows.Close()
+
+	columns, _ := rows.Columns()
+
+	results = append(results, columns)
+
+	for rows.Next() {
+		rowValues := make([]interface{}, len(columns))
+		for i := range columns {
+			rowValues[i] = new(sql.RawBytes)
+		}
+
+		rows.Scan(rowValues...)
+
+		var row []string
+		for _, col := range rowValues {
+			row = append(row, string(*col.(*sql.RawBytes)))
+		}
+
+		results = append(results, row)
+	}
+
+	return
+}
+
+func (db *MySQLCommand) GetRecords(table, where, sort string, offset, limit int) (paginatedResults [][]string, totalRecords int, err error) {
+	defaultLimit := 300
+
+	isPaginationEnabled := offset >= 0 && limit >= 0
+
+	if limit != 0 {
+		defaultLimit = limit
+	}
+
+	query := fmt.Sprintf("SELECT * FROM %s s LIMIT %d,%d", table, offset, defaultLimit)
+
+	if where != "" {
+		query = fmt.Sprintf("SELECT * FROM %s %s LIMIT %d,%d", table, where, offset, defaultLimit)
+	}
+
+	if sort != "" {
+		query = fmt.Sprintf("SELECT * FROM %s %s ORDER BY %s LIMIT %d,%d", table, where, sort, offset, defaultLimit)
+	}
+
+	paginatedRows, err := db.Connection.Query(query)
+	if err != nil {
+		return paginatedResults, totalRecords, err
+	}
+
+	if isPaginationEnabled {
+		queryWithoutLimit := fmt.Sprintf("SELECT COUNT(*) FROM %s %s", table, where)
+
+		rows := db.Connection.QueryRow(queryWithoutLimit)
+
+		if err != nil {
+			return paginatedResults, totalRecords, err
+		}
+
+		rows.Scan(&totalRecords)
+
+		defer paginatedRows.Close()
+	}
+
+	columns, _ := paginatedRows.Columns()
+
+	paginatedResults = append(paginatedResults, columns)
+
+	for paginatedRows.Next() {
+		rowValues := make([]interface{}, len(columns))
+		for i := range columns {
+			rowValues[i] = new(sql.RawBytes)
+		}
+
+		paginatedRows.Scan(rowValues...)
+
+		var row []string
+		for _, col := range rowValues {
+			row = append(row, string(*col.(*sql.RawBytes)))
+		}
+
+		paginatedResults = append(paginatedResults, row)
+
+	}
+
+	return
+}
+
+func (db *MySQLCommand) ExecuteQuery(query string) (results [][]string, err error) {
+	rows, err := db.Connection.Query(query)
+	if err != nil {
+		return results, err
+	}
+
+	defer rows.Close()
+
+	columns, _ := rows.Columns()
+
+	results = append(results, columns)
+
+	for rows.Next() {
+		rowValues := make([]interface{}, len(columns))
+		for i := range columns {
+			rowValues[i] = new(sql.RawBytes)
+		}
+
+		rows.Scan(rowValues...)
+
+		var row []string
+		for _, col := range rowValues {
+			row = append(row, string(*col.(*sql.RawBytes)))
+		}
+
+		results = append(results, row)
+
+	}
+
+	return
+}
+
+// TODO: Rewrites this logic to use the primary key instead of the id
+func (db *MySQLCommand) UpdateRecord(table, column, value, primaryKeyColumnName, primaryKeyValue string) error {
+	query := fmt.Sprintf("UPDATE %s SET %s = \"%s\" WHERE %s = \"%s\"", table, column, value, primaryKeyColumnName, primaryKeyValue)
+	_, err := db.Connection.Exec(query)
+
+	return err
+}
+
+// TODO: Rewrites this logic to use the primary key instead of the id
+func (db *MySQLCommand) DeleteRecord(table, primaryKeyColumnName, primaryKeyValue string) error {
+	query := fmt.Sprintf("DELETE FROM %s WHERE %s = \"%s\"", table, primaryKeyColumnName, primaryKeyValue)
+	_, err := db.Connection.Exec(query)
+
+	return err
+}
+
+func (db *MySQLCommand) ExecuteDMLStatement(query string) (result string, err error) {
+	res, error := db.Connection.Exec(query)
+
+	if error != nil {
+		return result, error
+	} else {
+		rowsAffected, _ := res.RowsAffected()
+
+		return fmt.Sprintf("%d rows affected", rowsAffected), error
+	}
+}
+
+func (db *MySQLCommand) ExecutePendingChanges(changes []DbDmlChange, inserts []DbInsert) (err error) {
+	queries := make([]string, 0, len(changes)+len(inserts))
+
+	// This will hold grouped changes by their RowId and Table
+	groupedUpdated := make(map[string][]DbDmlChange)
+	groupedDeletes := make([]DbDmlChange, 0, len(changes))
+
+	// Group changes by RowId and Table
+	for _, change := range changes {
+		switch change.Type {
+		case "UPDATE":
+			key := fmt.Sprintf("%s|%s|%s", change.Table, change.PrimaryKeyColumnName, change.PrimaryKeyValue)
+			groupedUpdated[key] = append(groupedUpdated[key], change)
+		case "DELETE":
+			groupedDeletes = append(groupedDeletes, change)
+		}
+	}
+
+	// Combine individual changes to SQL statements
+	for key, changes := range groupedUpdated {
+		columns := []string{}
+
+		// Split key into table and rowId
+		splitted := strings.Split(key, "|")
+		table := splitted[0]
+		primaryKeyColumnName := splitted[1]
+		primaryKeyValue := splitted[2]
+
+		for _, change := range changes {
+			columns = append(columns, fmt.Sprintf("%s='%s'", change.Column, change.Value))
+		}
+
+		// Merge all column updates
+		updateClause := strings.Join(columns, ", ")
+
+		query := fmt.Sprintf("UPDATE %s SET %s WHERE %s = '%s';", table, updateClause, primaryKeyColumnName, primaryKeyValue)
+
+		queries = append(queries, query)
+	}
+
+	for _, delete := range groupedDeletes {
+		statementType := ""
+		query := ""
+
+		statementType = "DELETE FROM"
+
+		query = fmt.Sprintf("%s %s WHERE %s = \"%s\"", statementType, delete.Table, delete.PrimaryKeyColumnName, delete.PrimaryKeyValue)
+
+		if query != "" {
+			queries = append(queries, query)
+		}
+	}
+
+	for _, insert := range inserts {
+		values := make([]string, 0, len(insert.Values))
+
+		for _, value := range insert.Values {
+			_, error := strconv.ParseFloat(value, 64)
+
+			if strings.ToLower(value) != "default" && error != nil {
+				values = append(values, fmt.Sprintf("\"%s\"", value))
+			} else {
+				values = append(values, value)
+			}
+		}
+
+		query := fmt.Sprintf("INSERT INTO %s (%s) VALUES (%s)", insert.Table, strings.Join(insert.Columns, ", "), strings.Join(values, ", "))
+
+		queries = append(queries, query)
+	}
+
+	tx, error := db.Connection.Begin()
+	if error != nil {
+		return error
+	}
+
+	for _, query := range queries {
+
+		_, err = tx.Exec(query)
+
+		if err != nil {
+			tx.Rollback()
+
+			return err
+		}
+	}
+
+	err = tx.Commit()
+
+	if err != nil {
+		return err
+	}
+
+	return err
+}
+
+func (db *MySQLCommand) SetProvider(provider string) {
+	db.Provider = provider
+}
+
+func (db *MySQLCommand) GetProvider() string {
+	return db.Provider
+}
---
+++ pkg/commands/sql.go
@@ -0,0 +1,59 @@
+package commands
+
+import (
+	"github.com/google/uuid"
+
+	"github.com/SpicyChickenFLY/lazysql/pkg/config"
+	"github.com/sirupsen/logrus"
+)
+
+type SqlCommand interface {
+	Connect(urlstr string) error
+	TestConnection(urlstr string) error
+	GetDatabases() ([]string, error)
+	GetTables(database string) (map[string][]string, error)
+	GetTableColumns(database, table string) ([][]string, error)
+	GetConstraints(table string) ([][]string, error)
+	GetForeignKeys(table string) ([][]string, error)
+	GetIndexes(table string) ([][]string, error)
+	GetRecords(table, where, sort string, offset, limit int) ([][]string, int, error)
+	UpdateRecord(table, column, value, primaryKeyColumnName, primaryKeyValue string) error
+	DeleteRecord(table string, primaryKeyColumnName, primaryKeyValue string) error
+	ExecuteDMLStatement(query string) (string, error)
+	ExecuteQuery(query string) ([][]string, error)
+	ExecutePendingChanges(changes []DbDmlChange, inserts []DbInsert) error
+	SetProvider(provider string)
+	GetProvider() string
+}
+
+// NewOSCommand os command runner
+func NewSqlCommand(log *logrus.Entry, config *config.AppConfig) SqlCommand {
+	return &MySQLCommand{}
+}
+
+type DbDmlChange struct {
+	Type                 string
+	Table                string
+	Column               string
+	Value                string
+	PrimaryKeyColumnName string
+	PrimaryKeyValue      string
+	Option               int
+}
+
+type DbInsert struct {
+	Table           string
+	Columns         []string
+	Values          []string
+	Option          int
+	PrimaryKeyValue uuid.UUID
+}
+
+type DbTableColumn struct {
+	Field   string
+	Type    string
+	Null    string
+	Key     string
+	Default string
+	Extra   string
+}
